0474b5269a9eb3d4be59c5c88bce062695021e5a
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-10-08 19:12:00 +0530

    Basic structure created

---
+++ pages/dashboard/index.js
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ pages/exam/[examId].js
@@ -0,0 +1,9 @@
+const ExamPage = () => {
+  return (
+    <div>
+      <h1>Exam Page</h1>
+    </div>
+  );
+};
+
+export default ExamPage;
