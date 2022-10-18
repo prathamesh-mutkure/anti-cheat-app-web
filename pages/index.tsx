7ac526acfa191c352fbb451aa0951c4d889cc496
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-10-18 22:45:27 +0530

    Added MUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      window.location.href = "/auth/login";
+    }
+  }, [isLoggedIn]);
+
   return (
     <div>
       <h1>Home Page</h1>
