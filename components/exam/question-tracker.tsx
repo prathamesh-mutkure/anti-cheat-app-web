cf506287a96dd1312dd2ed630560760a3c4472c4
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-11-07 14:19:28 +0530

    Improved Question Tracker

--- components/exam/question-tracker.tsx
+++ components/exam/question-tracker.tsx
@@ -1,4 +1,11 @@
-import { Avatar, Grid } from "@mui/material";
+import {
+  Avatar,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { examActions } from "../../store/exam-store";
@@ -10,12 +17,14 @@ interface QuestionCircleProps {
   questionNumber: number | string;
   onClick?: () => void;
   highlight?: boolean;
+  isAnswered: boolean;
 }
 
 const QuestionCircle: React.FC<QuestionCircleProps> = ({
   questionNumber,
   onClick,
   highlight = false,
+  isAnswered = false,
 }) => {
   return (
     <>
@@ -24,6 +33,7 @@ const QuestionCircle: React.FC<QuestionCircleProps> = ({
           onClick={onClick}
           sx={{
             border: highlight ? "solid 3px black" : "",
+            backgroundColor: isAnswered ? "green" : "red",
           }}
         >
           {questionNumber}
@@ -40,7 +50,7 @@ const QuestionTracker: React.FC<QuestionTrackerProps> = () => {
   // TODO: Maybe move highlight logic to circle component
 
   const dispatch = useAppDispatch();
-  const activeExam = useAppSelector((state) => state.exam.activeExam.exam);
+  const activeExam = useAppSelector((state) => state.exam.activeExam);
 
   const currentQuestion = useAppSelector(
     (state) => state.exam.activeExam.currentQuestion
@@ -50,25 +60,57 @@ const QuestionTracker: React.FC<QuestionTrackerProps> = () => {
     dispatch(examActions.goToQuestion(index));
   };
 
-  if (!activeExam) {
+  if (!activeExam.exam) {
     return <p>Error</p>;
   }
 
-  const { questionCount } = activeExam;
+  const { questionCount } = activeExam.exam;
 
   return (
-    <React.Fragment>
-      <Grid container rowSpacing={2} justifyContent="center">
-        {Array.from(Array(questionCount).keys()).map((i) => (
-          <QuestionCircle
-            key={i}
-            questionNumber={i + 1}
-            highlight={currentQuestion == i}
-            onClick={() => onClick(i)}
-          />
-        ))}
-      </Grid>
-    </React.Fragment>
+    <div>
+      <div className={classes.questionCircles}>
+        <Grid container rowSpacing={2} justifyContent="center">
+          {Array.from(Array(questionCount).keys()).map((i) => (
+            <QuestionCircle
+              key={i}
+              questionNumber={i + 1}
+              highlight={currentQuestion == i}
+              onClick={() => onClick(i)}
+              isAnswered={activeExam.answerKeys[i] !== null}
+            />
+          ))}
+        </Grid>
+      </div>
+
+      <div className={classes.questionCirclesLabel}>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <Avatar
+                sx={{
+                  backgroundColor: "green",
+                }}
+              >
+                {" "}
+              </Avatar>
+            </ListItemIcon>
+            <ListItemText>Answered</ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <Avatar
+                sx={{
+                  backgroundColor: "red",
+                }}
+              >
+                {" "}
+              </Avatar>
+            </ListItemIcon>
+            <ListItemText>Not Answered</ListItemText>
+          </ListItem>
+        </List>
+      </div>
+    </div>
   );
 };
 
