9992ef3c00e48fde4172b3be52a8b52acc8b4e59
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-11-11 21:27:38 +0530

    Dashboard Page Final and Mobile Friendly

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -2,16 +2,18 @@ import { Container, Grid } from "@mui/material";
 import { useAppSelector } from "../../hooks";
 import ExamCard from "./exam-card";
 import classes from "./dashboard.module.scss";
+import { useSession } from "next-auth/react";
 
 interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = () => {
   const assignedExams = useAppSelector((state) => state.exam.assignedExams);
-  const name = useAppSelector((state) => state.user.user.fname);
+
+  const session = useSession();
 
   return (
     <Container maxWidth="md" className={classes.container}>
-      <h1 className={classes.title}>Welcome {name}!</h1>
+      <h1 className={classes.title}>Welcome {session.data?.user?.fname}!</h1>
 
       <Grid container direction="column" spacing={4}>
         {assignedExams.map((exam) => (
