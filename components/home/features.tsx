63ac0c72718386e7990bbd70b18cfcb986b9ac9c
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-11-11 16:36:27 +0530

    Added Features and Theme

--- components/home/features.tsx
+++ components/home/features.tsx
@@ -12,6 +12,45 @@ import TrackChangesIcon from "@mui/icons-material/TrackChanges";
 
 interface FeaturesProps {}
 
+const featureList = [
+  {
+    icon: "",
+    title: "AI Face Detection",
+    content:
+      "We used AI and ML to detect cheating by tracking student’s facial movements",
+  },
+  {
+    icon: "",
+    title: "Cross Platform",
+    content: "This platform is availabe on Web, Android and iOS",
+  },
+  {
+    icon: "",
+    title: "Blocks Screen Capture",
+    content: "The mobile version of the app blocks any form of screen capture",
+  },
+  {
+    icon: "",
+    title: "Blocks App Exit",
+    content: "The user cannot exit the app or the website during exam",
+  },
+  {
+    icon: "",
+    title: "Assesment and Auditing",
+    content: "Support for instant assessment and auditing",
+  },
+  {
+    icon: "",
+    title: "Video Proctoring",
+    content: "Support for live video proctoring (future support)",
+  },
+  {
+    icon: "",
+    title: "Live Communication",
+    content: "Live real time communication with user (future support)",
+  },
+];
+
 const Features: React.FC<FeaturesProps> = () => {
   return (
     <section className={classes.featureSection}>
@@ -24,40 +63,34 @@ const Features: React.FC<FeaturesProps> = () => {
           columnSpacing={5}
           justifyContent="center"
         >
-          {Array(5)
-            .fill(0)
-            .map((i) => {
-              return (
-                <Grid item key={i}>
-                  <Card
+          {featureList.map((feature, i) => {
+            return (
+              <Grid item key={i}>
+                <Card
+                  sx={{
+                    maxWidth: "320px",
+                    backgroundColor: "white",
+                    borderRadius: "0.85rem",
+                    boxShadow: "0 5px 25px rgb(0 0 0 / 8%)",
+                    padding: "1rem",
+                  }}
+                >
+                  <CardMedia
                     sx={{
-                      maxWidth: "320px",
-                      backgroundColor: "white",
-                      borderRadius: "0.85rem",
-                      boxShadow: "0 5px 25px rgb(0 0 0 / 8%)",
-                      padding: "2rem",
+                      textAlign: "center",
                     }}
                   >
-                    <CardMedia>
-                      {/* <svg className="svgIcon">
-                      <use xlinkHref="#arrow-target-1"> </use>
-                    </svg> */}
+                    <TrackChangesIcon />
+                  </CardMedia>
 
-                      <TrackChangesIcon />
-                    </CardMedia>
-
-                    <CardContent>
-                      <h3>AI-enabled cheating detection</h3>
-                      <p>
-                        Lizards are a widespread group of squamate reptiles,
-                        with over 6,000 species, ranging across all continents
-                        except Antarctica
-                      </p>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              );
-            })}
+                  <CardContent>
+                    <h3>{feature.title}</h3>
+                    <p>{feature.content}</p>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </section>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,26 @@ import store from "../store/index";
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Montserrat", "Roboto", "Helvetica", "Arial", sans-serif`,
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps?.session}>
       <Provider store={store}>
-        <Head>
-          <title>Anti-Cheat Exam App</title>
-          <meta name="author" content="Prathamesh Mutkure" />
-        </Head>
-        <Component {...pageProps} />
-        <ToastContainer position="bottom-center" theme="light" />
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>Anti-Cheat Exam App</title>
+            <meta name="author" content="Prathamesh Mutkure" />
+          </Head>
+          <Component {...pageProps} />
+          <ToastContainer position="bottom-center" theme="light" />
+        </ThemeProvider>
       </Provider>
     </SessionProvider>
   );
