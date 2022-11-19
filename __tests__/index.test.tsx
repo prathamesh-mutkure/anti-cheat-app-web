56859dc23f2b8aa093562c463e0994212907ffcf
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-11-19 23:35:50 +0530

    Setup jest and testing

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import HomePage from "../pages/index";
+import "@types/jest";
+
+// describe("Home Page (Landing)...", () => {
+//   it("renders a heading", () => {
+// render(<HomePage />);
+// expect(true).toBe(true);
+//   });
+// });
